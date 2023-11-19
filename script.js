01aca0cb0ea17ff9b2515378c57febdc7a64a77f
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-19 10:45:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function makeid(l) {
   // write your code here
+	let num = parseInt(l);
+	let result = '';
+	let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for(let i = 0; i < num; i++){
+		result += characters.charAt(Math.random() * characters.length);
+	}
+	return result;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
+const l = prompt("Enter a number.");  2
 alert(makeid(l));
